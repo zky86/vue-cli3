0e28baf11cf68364cb63d3a9051a2e5229998ef6
zky <272495222@qq.com>
2020-04-13 14:19:40 +0800

    结构调整

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,4 +1,5 @@
 const index = () => import(/* webpackChunkName: "index" */ '@/views/index')
+const indexArticle = () => import(/* webpackChunkName: "indexArticle" */ '@/views/index/article')
 const indexList = () => import(/* webpackChunkName: "indexList" */ '@/views/index/list')
 const indexTabNav = () => import(/* webpackChunkName: "indexTabNav" */ '@/views/index/tabNav')
 const login = () => import(/* webpackChunkName: "login" */ '@/views/login')
@@ -13,6 +14,11 @@ export default [
     path: '/',
     component: index,
     children: [
+      {
+        name: 'index-article',
+        path: 'article',
+        component: indexArticle
+      },
       {
         name: 'index-list',
         path: 'list',
